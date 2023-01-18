6885544a5c99ec4fb516337d57eb7b1a71040ebe
Gilssilany Valentino <gilssilany@gmail.com>
2023-01-18 10:56:21 -0300

    formulario reativo

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,4 +1,7 @@
+import { NovoUsuarioService } from './novo-usuario.service';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { NovoUsuario } from './novo-usuario';
 
 @Component({
   selector: 'app-novo-usuario',
@@ -6,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./novo-usuario.component.css']
 })
 export class NovoUsuarioComponent {
+  novoUsuarioForm!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private novoUsuarioService: NovoUsuarioService
+  ){}
+
+  ngOnInit(): void{
+    this.novoUsuarioForm = this.formBuilder.group({
+      email: [''],
+      fullName: [''],
+      userName: [''],
+      password: [''],
+    });
+  }
+
+  cadastrar(){
+    const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
+  }
 
 }
